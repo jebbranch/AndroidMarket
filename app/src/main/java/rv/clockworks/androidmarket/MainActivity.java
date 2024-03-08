fcdcb2de633ace7e0ac8cb88a3c7aa5ad614bb99
huxiaoshi <huxiaoshi145@icloud.com>
2024-03-09 00:57:18 +0800

    1.0-1140 okhttp3框架引入

--- app/src/main/java/rv/clockworks/androidmarket/MainActivity.java
+++ app/src/main/java/rv/clockworks/androidmarket/MainActivity.java
@@ -1,26 +1,24 @@
 package rv.clockworks.androidmarket;
 
-import androidx.annotation.Keep;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import rv.clockworks.androidmarket.Service.KeepAliveService;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     Button btn=null;
+    TextView mian=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +26,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.about);
         btn.setOnClickListener(new About());
+        mian = findViewById(R.id.mian);
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url("https://www.baidu.com").build();
+        Request request = new Request.Builder().url("http://hxzbaka.hk3.345888.xyz.cdn.cloudflare.net/home.php?page=1").build();
         Call call = client.newCall(request);
         call.enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                //textView.setText("Failed to load content");
+                mian.setText("Failed to load content");
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 String html = response.body().string();
-                //textView.setText(html);
+                //mian.setText(html);
             }
         });
         //startService(new Intent(this, KeepAliveService.class));
